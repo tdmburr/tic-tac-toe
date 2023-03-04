5568967df07743e099e956bfaea6c864b3c5b7b4
Trey Marshall <marshall.trey92@yahoo.com>
2023-03-04 09:44:32 -0600

    refac: Change code in game file to be more dynamic and powerful for use in main.js

--- src/game.js
+++ src/game.js
@@ -2,13 +2,12 @@ class Game {
   constructor() {
     this.playerOne = new Player(1, "😺")
     this.playerTwo = new Player(2, "🐕")
-    this.boxID = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     this.draw = false
-    this.winner = false
+    this.winner = null
     this.turns = 0
-    this.startPlayer = "😺"
-    this.currentPlayerTurn = "😺"
+    this.currentPlayerTurn = this.playerOne
     this.gameInSession = true
+    this.startPlayer = 1
   }
   checkBoard() {
     var winConditions = [
@@ -26,53 +25,52 @@ class Game {
           this.playerOne.boardSpace.includes(winConditions[i][1]) && 
           this.playerOne.boardSpace.includes(winConditions[i][2])) 
       {
-          this.winner = true;
-          this.playerOne.wins ++
+          this.winner = this.playerOne
+          this.playerOne.wins += 1
           this.gameInSession = false;
       } else if (
           this.playerTwo.boardSpace.includes(winConditions[i][0]) && 
           this.playerTwo.boardSpace.includes(winConditions[i][1]) && 
           this.playerTwo.boardSpace.includes(winConditions[i][2])) 
       {
-          this.winner = true;
-          this.playerOne.wins ++
+          this.winner = this.playerTwo
+          this.playerTwo.wins += 1
           this.gameInSession = false;
       }
     }
+    this.checkDraw()
   }
 
   checkDraw() {
-    if (this.turns === 9 && this.winner === false) {
+    if (this.turns === 9 && this.winner === null) {
       this.draw = true
     }
   }
 
-  updateBoard(id) {
-    this.boxID[id] = this.currentPlayerTurn
-  }
-
-  changeTurn() {
-    if (this.currentPlayerTurn === "😺") {
-      this.currentPlayerTurn = "🐕"
-    } else {
-      this.currentPlayerTurn = "😺"
+  takeTurn(selectedBox) {
+    if (this.currentPlayerTurn === this.playerOne) {
+      this.playerOne.boardSpace.push(selectedBox)
+      this.currentPlayerTurn = this.playerTwo
+    } else if (this.currentPlayerTurn === this.playerTwo) {
+      this.playerTwo.boardSpace.push(selectedBox)
+      this.currentPlayerTurn = this.playerOne
     }
+    this.turns ++
   }
-  
+ 
   clearBoard() {
-    this.boxID = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     this.draw = false
-    this.winner = false
+    this.winner = null
     this.turns = 0
     this.gameInSession = true
     this.playerOne.boardSpace = []
     this.playerTwo.boardSpace = []
-    if (this.startPlayer === "😺") {
-      this.startPlayer = "🐕"
-      this.currentPlayerTurn = "🐕"
+    if (this.startPlayer === 1) {
+        this.currentPlayerTurn = this.playerTwo
+        this.startPlayer = 2
     } else {
-      this.startPlayer = "😺"
-      this.currentPlayerTurn = "😺"
+        this.currentPlayerTurn = this.playerOne
+        this.startPlayer = 1
     }
   }
 }
